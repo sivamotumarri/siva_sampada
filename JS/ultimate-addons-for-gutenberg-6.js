717ad5a9af1e7a3aa97ff72b6e9269b73e003c88
sivamotumarri <nageswar.motumarri@gmail.com>
2025-12-24 16:05:50 +0530

    Add files via upload
    Uploaded JS files

---
+++ JS/ultimate-addons-for-gutenberg-6.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCountdown.init( '.uagb-block-a377b579', {"block_id":"a377b579","endDateTime":"2024-08-11T20:54:00.000Z","showDays":true,"showHours":true,"showMinutes":true,"isFrontend":true,"timerEndAction":"zero","redirectURL":""} );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"a3324047","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"abc@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-a3324047', 229 );
+});
+ });
